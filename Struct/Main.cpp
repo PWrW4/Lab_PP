49a1b1b42c3e044f3726e89f3f82e6c0106b6512
wojtasiq-ideapad310 <wojtek.wroclaw@hotmail.com>
2017-01-23 21:46:01 +0100

    pplab przedostatnie

--- Struct/Main.cpp
+++ Struct/Main.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <stdio.h>
 
 #define N 100
 
@@ -22,7 +23,7 @@ struct KomisStruct
 
 
 int CountBaseSize(KomisStruct _CarDataBase[]) {
-	int L=0;
+	int L = 0;
 
 	for (int i = 0; i < N; i++)
 	{
@@ -32,33 +33,6 @@ int CountBaseSize(KomisStruct _CarDataBase[]) {
 
 	return L;
 }
-
-
-void SaveToFile(KomisStruct _CarDataBase[]) {
-	fstream plik("baza.txt", ios::out);
-	if (plik.good())
-	{
-		plik << "Marka \t\t Model \t\t Cena \t\t Przebieg" << "\n";
-		plik.flush();
-		cout << CountBaseSize(_CarDataBase) << endl;
-		for (int i = 0; i < N; i++)
-		{
-			if (!_CarDataBase[i].empty)
-			{
-				plik << _CarDataBase[i].Marka << " \t\t";
-				plik << _CarDataBase[i].Marka << " \t\t";
-				plik << _CarDataBase[i].ProdYear << " \t\t";
-				plik << _CarDataBase[i].Price;
-			}
-
-			plik << "\n";
-			plik.flush();
-		}
-		plik.close();
-		cout << "Saved to file" << endl;
-	}
-}
-
 
 
 
@@ -77,32 +51,41 @@ int SearchForEmpty(KomisStruct _CarDataBase[]) {
 
 
 void AddCarToDataBase(KomisStruct _CarDataBase[]) {
-	int i = SearchForEmpty(_CarDataBase);
-	cout << i;
-	if (i != -1) {
+	cout << "chcesz dodat cos do bazy" << endl;
+	string temp;
+	cin >> temp;
+	if (temp != "n")
+	{
+		int i = SearchForEmpty(_CarDataBase);
+		cout << i;
+		if (i != -1) {
 
-		cout << "Marka" << endl;
-		cin >> _CarDataBase[i].Marka;
+			cout << "Marka" << endl;
+			cin >> _CarDataBase[i].Marka;
 
-		cout << "Model" << endl;
-		cin >> _CarDataBase[i].Model;
+			cout << "Model" << endl;
+			cin >> _CarDataBase[i].Model;
 
-		cout << "ProdYear" << endl;
-		cin >> _CarDataBase[i].ProdYear;
+			cout << "ProdYear" << endl;
+			cin >> _CarDataBase[i].ProdYear;
 
-		cout << "Price" << endl;
-		cin >> _CarDataBase[i].Price;
+			cout << "Price" << endl;
+			cin >> _CarDataBase[i].Price;
 
-		_CarDataBase[i].empty = false;
+			_CarDataBase[i].empty = false;
 
 
 
-		cout << "Car Added" << endl;
+			cout << "Car Added" << endl;
 
+		}
+		else
+			cout << "error w addtodatabase;" << endl;
 	}
-	else
-		cout << "error;" << endl;
+	
+	
 }
+
 
 
 void ShowDataBase(KomisStruct _CarDataBase[], int i) {
@@ -134,16 +117,16 @@ void ViewDataBase(KomisStruct _CarDataBase[]) {
 
 		cin >> temp;
 
-		if (temp == ">" && i+1<=N) {
+		if (temp == ">" && i + 1 <= N) {
 			i++;
 		}
 		else {
-			if (temp == "<" && i-1 >= 0)
+			if (temp == "<" && i - 1 >= 0)
 			{
 				i--;
 			}
 			else {
-				if(temp == "d")
+				if (temp == "d")
 				{
 					_CarDataBase[i].empty = true;
 				}
@@ -151,17 +134,97 @@ void ViewDataBase(KomisStruct _CarDataBase[]) {
 
 		}
 	} while (temp != "q");
-	
+
 }
 
 
+//void SaveToFile(KomisStruct _CarDataBase[]) {
+//	fstream plik("baza.txt", ios::out);
+//	if (plik.good())
+//	{
+//		cout << "Uzyskano dostep do pliku!" << endl;
+//		//plik << "Marka \t\t Model \t\t Cena \t\t Przebieg" << "\n";
+//		plik.flush();
+//		plik << CountBaseSize(_CarDataBase) << endl;
+//		for (int i = 0; i < CountBaseSize(_CarDataBase); i++)
+//		{
+//			if (!_CarDataBase[i].empty) {
+//				plik << _CarDataBase[i].Marka << endl;
+//				plik << _CarDataBase[i].Model << endl;
+//				plik << _CarDataBase[i].ProdYear << endl;
+//				plik << _CarDataBase[i].Price << endl;
+//			}
+//
+//			//plik << "\n";
+//			plik.flush();
+//		}
+//		plik.close();
+//		cout << "Saved to file" << endl;
+//	}
+//}
+//
+//void TrytoLoad(KomisStruct _CarDataBase[])
+//{
+//	int basesize;
+//
+//	fstream plik("baza.txt", ios::in);
+//
+//	if (plik.good())
+//	{
+//		cout << "Uzyskano dostep do pliku!" << endl;
+//
+//		plik >> basesize;
+//		cout << basesize;
+//		for (int j=0; j < basesize; j++)
+//		{
+//			_CarDataBase[j].empty = false;
+//			plik >> _CarDataBase[j].Marka;
+//			plik >> _CarDataBase[j].Model;
+//			plik >> _CarDataBase[j].ProdYear;
+//			plik >> _CarDataBase[j].Price;
+//
+//		}
+//	}
+//	plik.close();
+//	cout<<"
+//
+//}
 
 
+void TrytoLoad(KomisStruct _CarDataBase[])
+{
+	int basesize = 0;
 
+	FILE *plik;
+	plik = fopen("baza.txt", "r");
+	if (plik!=NULL)
+	{
+		cout << "Uzyskano dostep do pliku!" << endl;
+
+		fscanf(plik, "%d", basesize);
+
+				for (int j=0; j < basesize; j++)
+				{
+					_CarDataBase[j].empty = false;
+					fscanf(plik, "%s%s%d%d", _CarDataBase[j].Model, _CarDataBase[j].Marka, _CarDataBase[j].ProdYear, _CarDataBase[j].Price);
+				}
+
+	}
+	fclose(plik);
+	cout << "otworzono" << endl;
+}
+
+void SaveToFile(KomisStruct _CarDataBase[])
+{
+
+}
 
 int main() {
 	KomisStruct CarDataBase[N];
 	KomisStruct Temp;
+
+	TrytoLoad(CarDataBase);
+
 
 	while (true)
 	{
